33ca119e03aa6a18b59b19c92ddb31688a05742f
Revi1337 <david122123@gmail.com>
2023-09-01 14:55:58 +0900

    Redirect Inflearn Query

---
+++ functions/inflearn.js
@@ -0,0 +1,59 @@
+const fetch = require("node-fetch");
+const querystring = require("querystring");
+const stringify = require("../utils/stringify.js");
+
+const INFLEARN_ORIGIN = "https://account.inflearn.com";
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json; charset=utf-8",
+};
+
+exports.handler = async (event) => {
+  const {
+    path,  
+    queryStringParameters,
+  } = event;
+
+  const url = new URL(path, INFLEARN_ORIGIN);
+  console.log(path);
+  const parameters = querystring.stringify({
+    ...queryStringParameters
+  });
+
+  // year=${year}
+  url.search = parameters;
+  
+  try {
+    const response = await fetch(url, {
+      headers: { 
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      credentials: 'include'
+    });
+
+    const body = await response.json();
+
+    if (body.error) {
+      return {
+        statusCode: body.error.code,
+        ok: false,
+        headers,
+        body: stringify(body),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      ok: true,
+      headers,
+      body: stringify(body),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      ok: false,
+      headers,
+      body: stringify(error),
+    };
+  }
+};
